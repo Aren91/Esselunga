4edcfe11515919042f73c079b33c9485e3c35835
matteomurino2@gmail.com <matteomurino2@gmail.com>
2024-02-23 12:37:05 +0100

    aggiunto completo UtenteEJB e interface inerente

--- EsselungaEJB/ejbModule/esselunga/ejb/controllers/UtenteEjb.java
+++ EsselungaEJB/ejbModule/esselunga/ejb/controllers/UtenteEjb.java
@@ -1,5 +1,61 @@
 package esselunga.ejb.controllers;
 
-public class UtenteEjb {
+import java.io.Serializable;
+import java.util.List;
+
+import esselunga.ejb.interfaces.UtenteEjbInterface;
+import esselunga.jpa.dao.UtenteDao;
+import esselunga.jpa.models.Utente;
+
+public class UtenteEjb implements Serializable, UtenteEjbInterface{
+
+	private static final long serialVersionUID = -1449442866644241177L;
+
+	@Override
+	public List<Utente> findAll() {
+		
+		UtenteDao utenteDao = new UtenteDao();
+		
+		List<Utente> listaUtenti = utenteDao.findAll();
+		
+		return listaUtenti;
+	}
+
+	@Override
+	public Utente findById(Integer id) {
+		
+		UtenteDao utenteDao = new UtenteDao();
+		
+		Utente utente = utenteDao.findById(id);
+		
+		return utente;
+	}
+
+	@Override
+	public Utente insert(Utente model) {
+
+		UtenteDao utenteDao = new UtenteDao();
+		
+		Utente utente = utenteDao.insert(model);
+		
+		return utente;
+	}
+
+	@Override
+	public Utente update(Utente model) {
+
+		UtenteDao utenteDao = new UtenteDao();
+		
+		Utente utente = utenteDao.update(model);
+		
+		return utente;
+	}
+
+	@Override
+	public void delete(Utente model) {
+
+		UtenteDao utenteDao = new UtenteDao();
+		utenteDao.delete(model);
+	}
 
 }
