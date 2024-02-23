ef1bd8dd4306623090d1028a4827635e4bf3b32f
Alessio Mammola <ale.mam91@proton.me>
2024-02-23 16:45:35 +0100

    annotations

--- EsselungaRest/src/esselunga/rest/ProdottoRest.java
+++ EsselungaRest/src/esselunga/rest/ProdottoRest.java
@@ -11,6 +11,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -83,6 +84,7 @@ public class ProdottoRest {
 	
 	@GET
 	@Path("/findProdottoById")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response findById(@QueryParam("id")Integer id) {
 		
 		logInit("findById", id);
